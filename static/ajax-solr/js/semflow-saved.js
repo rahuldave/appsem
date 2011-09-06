c24c77fa0bf23035537258111d55d198527fad84
Doug Burke <dburke@cfa.harvard.edu>
2011-09-06 08:59:06 -0400

    Moved a little bit of javascript from bodybody_saved template to semflow-saved

--- static/ajax-solr/js/semflow-saved.js
+++ static/ajax-solr/js/semflow-saved.js
@@ -2,3 +2,18 @@
  * Code for the saved page
  */
 
+/**
+ * Make a POST request to the ADS servers using the given
+ * URL path and apply the given callback to the response.
+ */
+var doADSproxy; // ugly polution of the global namespace
+
+(function ($) {
+
+    doADSproxy = function (urlpath, callback) {
+	$.post(SITEPREFIX + '/adsproxy',
+	       JSON.stringify({urlpath: urlpath}),
+	       callback);
+    }
+
+})(jQuery);
